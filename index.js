8945a1050e8c71e531c3cd4b2a34348ca6366769
roshannnn1 <roshan.adh84@gmail.com>
2022-07-11 20:00:55 -0500

    Removed function from a file and added that function into an existing js file

--- index.js
+++ index.js
@@ -17,3 +17,20 @@ function myFunction() {
       }
     }
   }
+
+  
+document.addEventListener("DOMContentLoaded", function(event) {
+document.documentElement.setAttribute("data-theme", "light");
+var themeSwitcher = document.getElementById("theme-switcher");
+ 
+themeSwitcher.onclick = function(){
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    
+    if(currentTheme === "light"){
+        var switchTo = "dark";
+    }
+    else 
+     switchTo = "light";
+    document.documentElement.setAttribute("data-theme", switchTo);
+}
+  });
