6e784c7aee1987e9452947a2802c21cde3e86b75
roshannnn1 <roshan.adh84@gmail.com>
2022-07-28 16:57:37 -0500

    Successfully implemented create recipe functionality

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
-
-
-
-
 //opens pop up for login and signup
 document.addEventListener('DOMContentLoaded', function() {
 
   var modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
-
+  
   var items = document.querySelectorAll('.collapsible');
   M.Collapsible.init(items);
-
-});
-
   
-document.addEventListener("DOMContentLoaded", function(event) {
-document.documentElement.setAttribute("data-theme", "light");
-var themeSwitcher = document.getElementById("theme-switcher");
- 
-themeSwitcher.onclick = function(){
+  });
+  
+  
+  document.addEventListener("DOMContentLoaded", function(event) {
+  document.documentElement.setAttribute("data-theme", "light");
+  var themeSwitcher = document.getElementById("theme-switcher");
+  
+  themeSwitcher.onclick = function(){
     var currentTheme = document.documentElement.getAttribute("data-theme");
     
     if(currentTheme === "light"){
@@ -27,5 +23,6 @@ themeSwitcher.onclick = function(){
     else 
     switchTo = "light";
     document.documentElement.setAttribute("data-theme", switchTo);
-}
+  }
   });
+
